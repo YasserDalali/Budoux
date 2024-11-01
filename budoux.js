d5092d23ae369dd8b97905b4431ef7ceee8d2476
YasserDalali <yasser.dalali.personal@gmail.com>
2024-11-01 12:28:56 +0100

    created a parsing funtion for p tag

--- budoux.js
+++ budoux.js
@@ -1,5 +1,9 @@
 import { loadDefaultJapaneseParser } from 'budoux';
+console.log("Budoux is integrated successfully")
+
 
 const parser = loadDefaultJapaneseParser();
-console.log(parser.parse('今日は天気です。'));
-// ['今日は', '天気です。']
+
+const contentElement = document.querySelector('p');  //! Will select the paragraph element
+const processedHTML = parser.translateHTMLString(contentElement.innerHTML); // Will translate the HTML content
+contentElement.innerHTML = processedHTML; // Replaces the innerHTML of the paragraph with the translated content
